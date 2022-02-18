accf9146f3ba93ffe2f94752e8cf293e3f965412
Otávio Augusto Chrispim De Paiva <otaviochrispim@gmail.com>
2022-02-18 15:47:19 -0300

    estrutura signin
    estrutura da pagina de signin, com inputs, botao e esqueci senha

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,20 +1,40 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import brandImg from "@assets/brand.png";
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+} from "./styles";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
-      <Button title="Entrar" type="secondary" />
+      {/* keyboardAvoidingView usado para dar padding quando clica no input, ou seja, a tela sobe */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
