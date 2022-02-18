68e8a9b88bed9791d35164c5e487e0f60f9e4de5
Otávio Augusto Chrispim De Paiva <otaviochrispim@gmail.com>
2022-02-18 10:08:23 -0300

    create signin
    criação básica da pagina de signin

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "styled-components/native";
 
 import theme from "./src/theme";
 
+import { SignIn } from "@screens/SignIn";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
@@ -16,5 +18,9 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  );
 }
