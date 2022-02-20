25b42f629687c0ccf4785d5b812600328b47ae80
Otávio Augusto Chrispim De Paiva <otaviochrispim@gmail.com>
2022-02-20 10:56:19 -0300

    header
    header da pagina de produtos

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Platform } from "react-native";
-import { Container } from "./styles";
+import { Platform, TouchableOpacity } from "react-native";
+import { Container, Header, Title, DeleteLabel } from "./styles";
 
 export function Product() {
   return (
-    <Container
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-    ></Container>
+    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+      <Header>
+        <Title>Cadastrar</Title>
+        <TouchableOpacity>
+          <DeleteLabel>Deletar</DeleteLabel>
+        </TouchableOpacity>
+      </Header>
+    </Container>
   );
 }
