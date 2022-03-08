fef33143a79daa9f0ea5706a6f2f5add654d2829
Otávio Augusto Chrispim De Paiva <otaviochrispim@gmail.com>
2022-03-08 16:31:59 -0300

    order form
    formulário de pedido

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -4,27 +4,64 @@ import { ButtonBack } from "@src/components/ButtonBack";
 import { RadioButton } from "@src/components/RadioButton";
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
 
-import { Container, Header, Photo, Sizes } from "./styles";
+import {
+  Container,
+  Header,
+  Photo,
+  Sizes,
+  Form,
+  Title,
+  Label,
+  InputGroup,
+  FormRow,
+  Price,
+  ContentScroll,
+} from "./styles";
+import { Input } from "@src/components/Input";
+import { Button } from "@src/components/Button";
 
 export function Order() {
   const [size, setSize] = useState("");
 
   return (
-    <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-      </Header>
-      <Sizes>
-        {PIZZA_TYPES.map((item) => (
-          <RadioButton
-            key={item.id}
-            title={item.name}
-            selected={size === item.id}
-            onPress={() => setSize(item.id)}
-          />
-        ))}
-      </Sizes>
-      <Photo source={{ uri: "http://github.com/otaviochrispim.png" }} />
-    </Container>
+    <ContentScroll>
+      <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
+        <Header>
+          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+        </Header>
+
+        <Photo source={{ uri: "http://github.com/otaviochrispim.png" }} />
+
+        <Form>
+          <Title>Nome da pizza</Title>
+          <Label>Selecione um tamanho</Label>
+          <Sizes>
+            {PIZZA_TYPES.map((item) => (
+              <RadioButton
+                key={item.id}
+                title={item.name}
+                selected={size === item.id}
+                onPress={() => setSize(item.id)}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Valor de R$60,00</Price>
+          <Button title="Confirmar pedido" />
+        </Form>
+      </Container>
+    </ContentScroll>
   );
 }
--- src/screens/Order/styles.ts
+++ src/screens/Order/styles.ts
@@ -30,3 +30,56 @@ export const Sizes = styled.View`
   justify-content: space-between;
   margin-bottom: 40px;
 `;
+
+export const Form = styled.View`
+  width: 100%;
+  margin-top: -120px;
+  padding: 24px;
+`;
+
+export const Title = styled.Text`
+  font-size: 32px;
+  margin-bottom: 32px;
+  text-align: center;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TITLE}
+    color: ${theme.COLORS.SECONDARY_900}
+  `}
+`;
+
+export const Label = styled.Text`
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.FONTS.TEXT}
+    color: ${theme.COLORS.SECONDARY_900}
+  `}
+`;
+
+export const FormRow = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputGroup = styled.View`
+  width: 48%;
+`;
+
+export const Price = styled.Text`
+  font-size: 14px;
+  margin: 24px 0;
+  align-self: flex-end;
+
+  ${({ theme }) => css`
+  font-family: ${theme.FONTS.TEXT}
+  color: ${theme.COLORS.SECONDARY_900}
+`}
+`;
+
+export const ContentScroll = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
